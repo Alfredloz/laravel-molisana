b489fe4465bd872193bf4141a25e089214e8698f
Alfredloz <alfredogregorio.lozano@gmail.com>
2021-02-11 17:09:40 +0100

    creazione card e divisione del tipo di card

--- resources/views/products/products.blade.php
+++ resources/views/products/products.blade.php
@@ -2,8 +2,21 @@
 <h1>Products</h1>
 @section('product-content')
     
-@foreach ($data as $product)
-    <h4> {{ $product['titolo'] }}</h4>
-    <p> {{ $product['descrizione'] }}</p>
-@endforeach
+<div class="product_content d_flex">
+    @foreach ($data as $product)
+        @if ($loop->first)
+            <h2 class="title">Le lunghe</h2>
+        @endif
+        @if ($loop->last)
+            <h2 class="title">Le cortissime</h2>
+        @endif
+        @if ($loop->iteration === 6)
+        <h2 class="title">Le corte</h2>
+        @endif
+        <div class="card">
+            <img src="{{ $product['src'] }}" alt="">
+            <h4> {{ $product['titolo'] }}</h4>
+        </div>
+    @endforeach
+</div>
 @endsection
